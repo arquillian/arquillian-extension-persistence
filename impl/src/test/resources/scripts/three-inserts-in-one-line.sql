03fff373b57882ba84fc6c07fa41990022216950
Bartosz Majsak <bartosz.majsak@gmail.com>
2012-08-12 20:31:57 +0200

    [ARQ-1065] Improved SQL script handling

---
+++ impl/src/test/resources/scripts/three-inserts-in-one-line.sql
@@ -0,0 +1,1 @@
+INSERT INTO useraccount (id, firstname, lastname, username, password) VALUES (1, 'John', 'Smith', 'doovde', 'password'); INSERT INTO useraccount (id, firstname, lastname, username, password) VALUES (2, 'Clark', 'Kent', 'superman', 'kryptonite'); INSERT INTO useraccount (id, firstname, lastname, username, password) VALUES (3, 'Cole', 'MacGrath', 'infamous', 'kessler');
---
+++ impl/src/test/resources/scripts/two-inserts-with-comment.sql
@@ -0,0 +1,4 @@
+-- Simple insert statements
+INSERT INTO useraccount (id, firstname, lastname, username, password) VALUES (1, 'John', 'Smith', 'doovde', 'password');
+-- Simple insert statements
+INSERT INTO useraccount (id, firstname, lastname, username, password) VALUES (2, 'Clark', 'Kent', 'superman', 'kryptonite');
