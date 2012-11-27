9afa40d5694724eb03912fd889faa43735ba501d
Cyrill Rüttimann <ruettimac@me.com>
2012-11-27 21:54:17 +0100

    When we define columns for sorting in the orderBy we do not add other columns anymore. When we define global column names and this columns is not available on all tables (for example join tables), we log a warning but do not fail comparing the dataset.

--- impl/src/main/java/org/jboss/arquillian/persistence/dbunit/DataSetComparator.java
+++ impl/src/main/java/org/jboss/arquillian/persistence/dbunit/DataSetComparator.java
@@ -21,11 +21,7 @@ import static org.jboss.arquillian.persistence.dbunit.DataSetUtils.extractColumn
 import static org.jboss.arquillian.persistence.dbunit.DataSetUtils.extractColumnsNotSpecifiedInExpectedDataSet;
 import static org.jboss.arquillian.persistence.dbunit.DataSetUtils.extractNonExistingColumns;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.logging.Logger;
 
 import org.dbunit.Assertion;
@@ -144,15 +140,37 @@ public class DataSetComparator
          throws DataSetException
    {
       final List<String> columnsForSorting = new ArrayList<String>();
-      columnsForSorting.addAll(orderBy.global);
+      columnsForSorting.addAll(existingColumnsForTable(expectedDataSet, tableName, orderBy.global));
       final List<String> columsPerTable = orderBy.columnsPerTable.get(tableName);
       if (columsPerTable != null)
       {
          columnsForSorting.addAll(columsPerTable);
       }
-      columnsForSorting.addAll(additionalColumnsForSorting(expectedDataSet.getTable(tableName),
-            currentDataSet.getTable(tableName)));
+
+      // Add all columns if none are defined in orderBy
+      if (columnsForSorting.isEmpty()) {
+         columnsForSorting.addAll(additionalColumnsForSorting(expectedDataSet.getTable(tableName),
+                 currentDataSet.getTable(tableName)));
+      }
+
       return columnsForSorting;
+   }
+
+   private Collection<String> existingColumnsForTable(IDataSet set, String tableName, List<String> expectedColumns) throws DataSetException
+   {
+      Set<String> existingColumns = new HashSet<String>();
+
+      Collection<String> tableColumns = extractColumnNames(set.getTable(tableName).getTableMetaData().getColumns());
+
+      for (String currentColumn : expectedColumns) {
+         if (tableColumns.contains(currentColumn.toLowerCase())) {
+            existingColumns.add(currentColumn.toLowerCase());
+         } else {
+            log.warning("Column " + currentColumn.toLowerCase() + " in Table " + tableName + " was set to be an ordering column but does not exist in this table. Ignoring it. Consider using [TABLENAME.COLUMNNAME]");
+         }
+      }
+
+      return existingColumns;
    }
 
    private static <T> String[] toArray(final List<T> list)
