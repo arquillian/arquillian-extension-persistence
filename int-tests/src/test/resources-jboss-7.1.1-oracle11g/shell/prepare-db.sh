7cf03ed430a19ebc8c89657daecfe604cfcfa38a
Bartosz Majsak <bartosz.majsak@gmail.com>
2016-04-17 20:54:00 +0200

    Fixed docker setup for oracle-11g

--- int-tests/src/test/resources-jboss-7.1.1-oracle11g/shell/prepare-db.sh
+++ int-tests/src/test/resources-jboss-7.1.1-oracle11g/shell/prepare-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
-export PATH=$ORACLE_HOME/bin:$PATH
+export PATH=${ORACLE_HOME}/bin:$PATH
 export ORACLE_SID=XE
 
 rm -f ape.sql
--- int-tests/src/test/resources-jboss-7.1.1-oracle11g/shell/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sshpass -p admin ssh -v -t -oStrictHostKeyChecking=no root@localhost -p 49160 < $1
