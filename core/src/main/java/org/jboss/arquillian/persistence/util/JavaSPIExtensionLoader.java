32ee5386131c4a53b9bbfa1dc42b2c4c7b00944e
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-01-08 17:39:17 +0100

    Updates Arquillian Core to lates release 1.1.12.Final

--- core/src/main/java/org/jboss/arquillian/persistence/util/JavaSPIExtensionLoader.java
+++ core/src/main/java/org/jboss/arquillian/persistence/util/JavaSPIExtensionLoader.java
@@ -5,13 +5,18 @@ import org.jboss.arquillian.core.spi.LoadableExtension;
 import org.jboss.arquillian.core.spi.Validate;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Collection;
 import java.util.Enumeration;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 /**
  * ServiceLoader implementation that use META-INF/services/interface files to registered Services.
@@ -25,6 +30,7 @@ public class JavaSPIExtensionLoader implements ExtensionLoader {
     //-------------------------------------------------------------------------------------||
 
     private static final String SERVICES = "META-INF/services";
+    private static final String EXCLUSIONS = "META-INF/exclusions";
 
     //-------------------------------------------------------------------------------------||
     // Required Implementations - ExtensionLoader -----------------------------------------||
@@ -32,7 +38,12 @@ public class JavaSPIExtensionLoader implements ExtensionLoader {
 
     @Override
     public Collection<LoadableExtension> load() {
-        return all(JavaSPIExtensionLoader.class.getClassLoader(), LoadableExtension.class);
+        return all(org.jboss.arquillian.core.impl.loadable.JavaSPIExtensionLoader.class.getClassLoader(), LoadableExtension.class);
+    }
+
+    @Override
+    public Map<Class<?>, Set<Class<?>>> loadVetoed() {
+        return loadVetoed(org.jboss.arquillian.core.impl.loadable.JavaSPIExtensionLoader.class.getClassLoader());
     }
 
     //-------------------------------------------------------------------------------------||
@@ -48,15 +59,95 @@ public class JavaSPIExtensionLoader implements ExtensionLoader {
                 load(serviceClass, classLoader));
     }
 
+    /**
+     * This method first finds all files that are in claspath placed at META-INF/exclusions
+     * Each of this file has a name that represents the service type that needs to veto.
+     * The content of this file is a list of real implementations that you want to veto.
+     *
+     * @return List of vetos
+     */
+    public Map<Class<?>, Set<Class<?>>> loadVetoed(ClassLoader classLoader) {
+
+        Validate.notNull(classLoader, "ClassLoader must be provided");
+
+        final Map<Class<?>, Set<Class<?>>> vetoed = new LinkedHashMap<Class<?>, Set<Class<?>>>();
+
+        try {
+            final Enumeration<URL> exclusions = classLoader.getResources(EXCLUSIONS);
+
+            while (exclusions.hasMoreElements()) {
+                URL exclusion = exclusions.nextElement();
+                Properties vetoedElements = new Properties();
+                final InputStream inStream = exclusion.openStream();
+
+                try {
+                    vetoedElements.load(inStream);
+
+                    final Set<Map.Entry<Object, Object>> entries = vetoedElements.entrySet();
+
+                    for (Map.Entry<Object, Object> entry : entries) {
+                        String service = (String) entry.getKey();
+                        String serviceImpls = (String) entry.getValue();
+
+                        addVetoedClasses(service, serviceImpls, classLoader, vetoed);
+
+                    }
+
+                } finally {
+                    if (inStream != null) {
+                        inStream.close();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load exclusions from " + EXCLUSIONS, e);
+        }
+
+        return vetoed;
+    }
+
     //-------------------------------------------------------------------------------------||
     // Internal Helper Methods - Service Loading ------------------------------------------||
     //-------------------------------------------------------------------------------------||
 
+    private void addVetoedClasses(String serviceName, String serviceImpls, ClassLoader classLoader, Map<Class<?>, Set<Class<?>>> vetoed) {
+        try {
+            final Class<?> serviceClass = classLoader.loadClass(serviceName);
+            final Set<Class<?>> classes = loadVetoedServiceImpl(serviceImpls, classLoader);
+
+            final Set<Class<?>> registeredVetoedClasses = vetoed.get(serviceClass);
+            if (registeredVetoedClasses == null) {
+                vetoed.put(serviceClass, classes);
+            } else {
+                registeredVetoedClasses.addAll(classes);
+            }
+
+        } catch (ClassNotFoundException e) {
+            // ignores since this is a veto that it is not applicable
+        }
+    }
+
+    private Set<Class<?>> loadVetoedServiceImpl(String serviceImpls, ClassLoader classLoader) {
+
+        final StringTokenizer serviceImplsSeparator = new StringTokenizer(serviceImpls, ",");
+        final Set<Class<?>> serviceImplsClass = new LinkedHashSet<Class<?>>();
+
+        while (serviceImplsSeparator.hasMoreTokens()) {
+            try {
+                serviceImplsClass.add(classLoader.loadClass(serviceImplsSeparator.nextToken().trim()));
+            } catch (ClassNotFoundException e) {
+                // ignores since this is a veto that it is not applicable
+            }
+        }
+
+        return serviceImplsClass;
+    }
+
     private <T> Set<Class<? extends T>> load(Class<T> serviceClass, ClassLoader loader) {
         String serviceFile = SERVICES + "/" + serviceClass.getName();
 
-        LinkedHashSet<Class<? extends T>> providers = new LinkedHashSet<Class<? extends T>>();
-        LinkedHashSet<Class<? extends T>> vetoedProviders = new LinkedHashSet<Class<? extends T>>();
+        Set<Class<? extends T>> providers = new LinkedHashSet<Class<? extends T>>();
+        Set<Class<? extends T>> vetoedProviders = new LinkedHashSet<Class<? extends T>>();
 
         try {
             Enumeration<URL> enumeration = loader.getResources(serviceFile);
@@ -131,11 +222,6 @@ public class JavaSPIExtensionLoader implements ExtensionLoader {
      * <p>
      * Verifies that the found ServiceImpl implements Service.
      *
-     * @param <T>
-     * @param serviceType The Service interface
-     * @param className   The name of the implementation class
-     * @param loader      The ClassLoader to load the ServiceImpl from
-     * @return A new instance of the ServiceImpl
      * @throws Exception If problems creating a new instance
      */
     private <T> T createInstance(Class<? extends T> serviceImplClass) {
