b6301f8ddf23555a34564fc370b7a4e56e7480b3
Bartosz Majsak <bartosz.majsak@gmail.com>
2017-05-30 18:39:46 +0200

    fix(test): temporarily ignores postman ftest due to travis issue

--- arquillian-ape-rest/postman-ftest/src/test/java/org/arquillian/ape/rest/postman/PostmanTest.java
+++ arquillian-ape-rest/postman-ftest/src/test/java/org/arquillian/ape/rest/postman/PostmanTest.java
@@ -8,6 +8,7 @@ import org.arquillian.cube.HostIp;
 import org.arquillian.cube.HostPort;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,6 +17,7 @@ import static org.hamcrest.CoreMatchers.is;
 
 @RunWith(Arquillian.class)
 @HealthCheck("/message")
+@Ignore("FIXME this fails on travis-ci due to docker/cube not able to start/connect to the container: http://bit.ly/2r82st1")
 public class PostmanTest {
 
     @ArquillianResource
